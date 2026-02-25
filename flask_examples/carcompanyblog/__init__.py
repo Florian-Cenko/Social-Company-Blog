5893a4b817829129501a056f8054793e709e11bc
Florian Cenko <cenkoflorian@gmail.com>
2026-02-25 12:59:00 +0200

    Adding Error Pages

--- flask_examples/carcompanyblog/__init__.py
+++ flask_examples/carcompanyblog/__init__.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 
 from carcompanyblog.core.views import core #import blueprint
+from carcompanyblog.error_pages.handlers import error_pages #import blueprint
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
